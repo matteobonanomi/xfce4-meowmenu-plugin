c06d7a6a35ecd41f1986c06195d31e85348d609b
Matteo Bonanomi <mbonanomi.dev@proton.me>
2026-05-14 12:17:37 +0200

    fix Apps/Places button position and minor UI bugs

--- panel-plugin/window.h
+++ panel-plugin/window.h
@@ -173,6 +173,8 @@ private:
 	bool m_places_active;
 	bool m_mode_switch_in_progress;
 	gulong m_places_property_slot;
+	GtkWidget* m_mode_selector_separator;
+	std::vector<GtkWidget*> m_app_category_widgets;
 
 	GtkScrolledWindow* m_sidebar;
 	GtkBox* m_category_buttons;
